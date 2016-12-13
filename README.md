83e54eb6b42f41bb19a36a65a10a05e567799b7b
songlairui <cnre@live.cn>
2016-12-14 00:42:28 +0800

    init file

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# Coding-Show-vue
-使用vue展现的前端
+# coding-show-vue
+
+> Vue version Coding-Show
+
+## 已创建文件结构
+文件结构：
+```
+src/
+  components/
+    header.vue           [init]
+    footer.vue           [init]
+    searchBar.vue        [init]
+    adminTitle.vue       [init]
+    adminSkill.vue
+    adminRequest.vue
+    adminProfessor.vue
+    userLogin.vue
+    userReg.vue
+    index.vue
+    publish.vue
+    develop.vue
+    detail.vue
+    add.vue              [larry]
+    modi.vue             [larry]
+```
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+```
+
+已安装: vue-router、vuex、bootstrap、axios
---
+++ src/app.vue
@@ -0,0 +1,60 @@
+<template>
+  <div id="wrapper">
+    <header-component>
+      <ul class="nav navbar-nav" slot='nav-left'>
+        <li><a class="navbar-brand" href="#" v-text='sitename'>Coding-Show</a></li>
+        <li>
+          <router-link to='/'>开源项目</router-link>
+        </li>
+        <li>
+          <router-link to='/'>运营的项目</router-link>
+        </li>
+        <li>
+          <router-link to='/'>专业用户入口</router-link>
+        </li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right" slot='nav'>
+        <li>
+          <router-link to='/add'>添加项目</router-link>
+        </li>
+        <li>
+          <router-link to='/publish'>发布的项目</router-link>
+        </li>
+        <li>
+          <router-link to='/develop'>开发的项目</router-link>
+        </li>
+        <li>
+          <router-link to='/reg'>注册</router-link>
+        </li>
+        <li>
+          <router-link to='/login'>登入</router-link>
+        </li>
+      </ul>
+    </header-component>
+    <section class="container">
+      <div class="col-xs-10 col-xs-offset-1">
+        <router-view></router-view>
+      </div>
+    </section>
+    <footer-component></footer-component>
+  </div>
+</template>
+
+<script>
+import HeaderComponent from './components/header'
+import FooterComponent from './components/footer'
+require('../node_modules/bootstrap/dist/css/bootstrap.min.css')
+
+export default {
+  name: 'app',
+  data () {
+    return{
+      sitename: 'Coding-Show'
+    }
+  },
+  components: {
+    HeaderComponent,
+    FooterComponent
+  }
+}
+</script>
