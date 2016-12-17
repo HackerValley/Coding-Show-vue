5b8a75ee8627ef1a254f387472e8b5f40019d3cd
slimwang <netbot@vip.qq.com>
2016-12-17 12:37:08 +0800

    vue

--- src/components/adminRequest.vue
+++ src/components/adminRequest.vue
@@ -1,3 +1,110 @@
+<style>
+
+.fr {
+    float: right
+}
+
+.search-box {
+    margin: 20px auto 40px;
+}
+
+</style>
+
 <template>
-  <p>【Blank Content】</p>
+
+<div class="container">
+    <div class="row">
+        <h2>
+            后台管理-项目审核
+             <span class="fr button-top">
+               <a href="#" title="expert" class="btn btn-info">专家审核</a>
+               <a href="#" title="skill" class="btn btn-info">技能审核</a>
+               <a href="#" title="project" class="btn btn-info">项目审核</a>
+               <a href="#" title="index" class="btn btn-info">返回首页</a>
+             </span>
+          </h2>
+        <hr>
+    </div>
+    <div class="row">
+        <div class="col-xs-10 col-xs-offset-1 col-md-8 col-md-offset-2">
+            <div class="row search-box">
+                <div class="col-xs-10 col-xs-offset-1 col-md-6 col-md-offset-3">
+                    <div class="input-group">
+                        <input type="text" class="form-control" name="" value="" placeholder="请在此搜索姓名">
+                        <span class="input-group-btn">
+                  <input type="button" name="" value="搜索" class="btn btn-default" >
+                </span>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <th>姓名</th>
+                            <th>联系电话</th>
+                            <th>微信</th>
+                            <th>项目名称</th>
+                            <th>操作</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-repeat="applicants">
+                            <td>{{ name }}</td>
+                            <td>{{ telephone }}</td>
+                            <td>{{ wechat }}</td>
+                            <td>{{ project_name }}</td>
+                            <td>
+                                <button class="btn btn-default btn-xs" type="button" name="button">通过</button>
+                                <button class="btn btn-default btn-xs" type="button" name="button">拒绝</button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
+
 </template>
+
+<script>
+
+export default {
+    data() {
+        return {
+            data: {
+                applicants: [{
+                    'name': '关毅然',
+                    'telephone': '18610199913',
+                    'wechat': 'yiranguan',
+                    'project_name': '旅行app',
+                    'operation': ''
+                }, {
+                    'name': '关毅然',
+                    'telephone': '18610199913',
+                    'wechat': 'yiranguan',
+                    'project_name': '旅行app',
+                    'operation': ''
+                }, {
+                    'name': '关毅然',
+                    'telephone': '18610199913',
+                    'wechat': 'yiranguan',
+                    'project_name': '旅行app',
+                    'operation': ''
+                }, {
+                    'name': '关毅然',
+                    'telephone': '18610199913',
+                    'wechat': 'yiranguan',
+                    'project_name': '旅行app',
+                    'operation': ''
+                }]
+            }
+
+        }
+    }
+
+
+}
+
+</script>
