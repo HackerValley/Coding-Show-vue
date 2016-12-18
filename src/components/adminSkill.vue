a9f26520bbdb5a9f14f869d0524002777c3b0a45
songlairui-laptop <cnre@live.cn(cnre)>
2016-12-18 19:29:30 +0800

    modify name of router

--- src/components/adminSkill.vue
+++ src/components/adminSkill.vue
@@ -1,68 +1,101 @@
 <template>
-  <div>
+  <div class="col-xs-10 col-xs-offset-1">
+    <admin-title></admin-title>
     <div class="row">
-      <h2>
-        后台管理
-         <small class="fr button-top">
-           <a href="#"><button type="button" name="expert" class="btn btn-primary">专家审核</button></a>
-           <a href="#"><button type="button" name="skill" class="btn btn-primary">技能审核</button></a>
-           <a href="#"><button type="button" name="project" class="btn btn-primary">项目审核</button></a>
-           <a href="#"><button type="button" name="index" class="btn btn-primary">返回首页</button></a>
-         </small>
-      </h2>
-      <hr>
-    </div>
-    <div class="row">
-      <div class="col-xs-1"></div>
-      <div class="col-xs-10">
-        <form class="text-center" action="/search-name" method="post">
-          <input type="text" name="" value="" placeholder="请在此搜索姓名">
-          <input type="button" name="" value="搜索" class="btn btn-sm btn-primary" >
-        </form>
-        <ul>
-          <li>
-            <p>
-              name&nbsp;&nbsp;&nbsp;18600000000&nbsp;&nbsp;&nbsp;微信：xxxxxxxxxxxx&nbsp;&nbsp;&nbsp;前端
-              <span class="fr">
-                <button type="button" name="pass" class="btn btn-xs btn-warning">通过</button>
-                <button type="button" name="reject" class="btn btn-xs btn-warning">拒绝</button>
-              </span>
-            </p>
-            <hr>
-          </li>
-          <li>
-            <p>
-              name&nbsp;&nbsp;&nbsp;18600000000&nbsp;&nbsp;&nbsp;微信：xxxxxxxxxxxx&nbsp;&nbsp;&nbsp;前端
-              <span class="fr">
-                <button type="button" name="pass" class="btn btn-xs btn-warning">通过</button>
-                <button type="button" name="reject" class="btn btn-xs btn-warning">拒绝</button>
-              </span>
-            </p>
-            <hr>
-          </li>
-          <li>
-            <p>
-              name&nbsp;&nbsp;&nbsp;18600000000&nbsp;&nbsp;&nbsp;微信：xxxxxxxxxxxx&nbsp;&nbsp;&nbsp;前端
-              <span class="fr">
-                <button type="button" name="pass" class="btn btn-xs btn-warning">通过</button>
-                <button type="button" name="reject" class="btn btn-xs btn-warning">拒绝</button>
-              </span>
-            </p>
-            <hr>
-          </li>
-          <li>
-            <p>
-              name&nbsp;&nbsp;&nbsp;18600000000&nbsp;&nbsp;&nbsp;微信：xxxxxxxxxxxx&nbsp;&nbsp;&nbsp;前端
-              <span class="fr">
-                <button type="button" name="pass" class="btn btn-xs btn-warning">通过</button>
-                <button type="button" name="reject" class="btn btn-xs btn-warning">拒绝</button>
-              </span>
-            </p>
-            <hr>
-          </li>
-        </ul>
+      <div class="col-xs-10 col-xs-offset-1 col-md-8 col-md-offset-2">
+        <div class="row search-box">
+          <div class="col-xs-10 col-xs-offset-1 col-md-6 col-md-offset-3">
+            <div class="input-group">
+              <input type="text" class="form-control" name="" value="" placeholder="请在此搜索姓名">
+              <span class="input-group-btn">
+                  <input type="button" name="" value="搜索" class="btn btn-default" >
+                </span>
+            </div>
+          </div>
+        </div>
+        <div class="list-group skills">
+          <div class="list-group-item">
+            <span class="fr">
+              <a href="#" class="btn btn-xs btn-warning">通过</a>
+              <a href="#" class="btn btn-xs btn-warning">拒绝</a>
+            </span>
+            <span class="name">xxxxx</span>
+            <span class="wechat">xxxxx</span>
+            <span class="skill">前端</span>
+          </div>
+          <div class="list-group-item">
+            <span class="fr">
+              <a href="#" class="btn btn-xs btn-warning">通过</a>
+              <a href="#" class="btn btn-xs btn-warning">拒绝</a>
+            </span>
+            <span class="name">xxxxx</span>
+            <span class="wechat">xxxxx</span>
+            <span class="skill">前端</span>
+          </div>
+          <div class="list-group-item">
+            <span class="fr">
+              <a href="#" class="btn btn-xs btn-warning">通过</a>
+              <a href="#" class="btn btn-xs btn-warning">拒绝</a>
+            </span>
+            <span class="name">xxxxx</span>
+            <span class="wechat">xxxxx</span>
+            <span class="skill">前端</span>
+          </div>
+          <div class="list-group-item">
+            <span class="fr">
+              <a href="#" class="btn btn-xs btn-warning">通过</a>
+              <a href="#" class="btn btn-xs btn-warning">拒绝</a>
+            </span>
+            <span class="name">xxxxx</span>
+            <span class="wechat">xxxxx</span>
+            <span class="skill">前端</span>
+          </div>
+        </div>
       </div>
-      <div class="col-xs-1"></div>
     </div>
   </div>
 </template>
+<script>
+import AdminTitle from './adminTitle.vue'
+
+export default {
+  components: {
+    AdminTitle
+  }
+}
+</script>
+<style type="text/css">
+.fr {
+  float: right
+}
+
+.button-top {
+  margin-bottom: 1px;
+}
+
+.search-box {
+  margin: 20px auto 40px;
+}
+
+.skills span {
+  display: inline-block;
+}
+
+.skills span.name,
+.skills span.wechat,
+.skills span.skill {
+  padding-right: 2em;
+}
+
+.skills span.name::before {
+  content: 'name: '
+}
+
+.skills span.wechat::before {
+  content: '微信: '
+}
+
+.skills span.skill::before {
+  content: ' '
+}
+</style>
