b3b9a9b7ad8594b4ac218042767f7144135dbd5b
songlairui <cnre@live.cn>
2017-01-08 18:30:29 +0800

    注册页面，增加信息提示UI

--- src/components/userReg.vue
+++ src/components/userReg.vue
@@ -8,10 +8,11 @@
           <div class="col-xs-10 col-xs-offset-1">
               <form action="" role="form" class="form-horizontal">
                   <div class="form-group">
-                      <label for="userName" class="col-xs-3 col-sm-2 control-label ">姓名</label>
+                      <label for="userName" class="col-xs-3 col-sm-2 control-label ">用户名</label>
                       <div class="col-xs-9 col-sm-8 col-md-6">
                           <input type="text" v-model='formdata.username' name="userName" id="userName" placeholder="您的姓名" class="form-control">
-                          <p class="form-control-static text-muted">真实姓名</p>
+                          <p class="form-control-static text-muted">用户名</p>
+                          <p class="form-control-static alert alert-danger" v-if='displayalerts && alerts.data.username !== true'>{{ alerts.data.username }}</p>
                       </div>
                   </div>
                   <div class="form-group">
@@ -19,6 +20,7 @@
                     <div class="col-xs-9 col-sm-8 col-md-6">
                       <input type="text" name="email" id="email" placeholder="邮箱地址" class="form-control" v-model='formdata.email'>
                       <p class="form-control-static text-muted">邮箱地址</p>
+                      <p class="form-control-static alert alert-danger" v-if='displayalerts && alerts.data.email !== true'>{{ alerts.data.email }}</p>
                     </div>
                   </div>
                   <div class="form-group">
@@ -40,6 +42,7 @@
                       <div class="col-xs-9 col-md-8">
                           <input type="password" v-model='formdata.pwd' name="pwd" id="pwd" placeholder="密码" class="form-control">
                           <p class="form-control-static text-muted">请牢记</p>
+                          <p class="form-control-static alert alert-danger" v-if='displayalerts && alerts.data.pwd !== true'>{{ alerts.data.pwd }}</p>
                       </div>
                   </div>
                   <div class="form-group">
@@ -47,7 +50,7 @@
                       <div class="col-xs-9 col-md-8">
                           <input type="password" v-model='formdata.pwdconfirm' name="pwdconfirm" id="pwdconfirm" placeholder="请再次输入密码" class="form-control">
                           <p class="form-control-static text-muted">和输入密码一致</p>
-                          <p class="form-control-static alert alert-warning" v-if='!init & alerts.pwd != 1'>{{ alerts.pwd }}</p>
+                          <p class="form-control-static alert alert-danger" v-if="alerts.data.pwd === '两次输入不一致'">{{ alerts.data.pwd }}</p>
                       </div>
                   </div>
                   <div class="form-group">
@@ -167,26 +170,37 @@
                   <div class="row imgpool">
                       <div class="col-xs-9 col-xs-offset-3 col-sm-10 col-sm-offset-2"></div>
                   </div>
-                  <div class="alert alert-danger" role="alert" v-if='submited'>{{ alerts.form }}</div>
                   <div class="from-group form-group-lg text-right">
 
                       <hr>
+                      <a class="btn btn-lg btn-info" @click.prevent='toastOne()'>添加然后消失</a>
                       <a class="btn btn-lg btn-info" @click.prevent='register()'>提交</a>
                   </div>
               </form>
           </div>
       </div>
+      <div class="row tip">
+        <transition-group name='a-complete'>
+          <div :class="'alert-' + tip.type" class="alert alert-dismissible a-complete-item" role="alert" :key='tip.id' v-for='(tip,idx) in tips'>
+            <strong>.</strong> {{ tip.message }}.
+          </div>
+        </transition-group>
+      </div>
   </div>
 </template>
 <script>
   import * as api from '../api/request'
+  import Tip from './common/tip.vue'
 
   export default {
-    data () {
+    components: {
+      Tip
+    },
+    data (){
       return {
+        timers: {},
         msg: 'init msg',
-        init: false,
-        submited: false,
+        displayalerts: false, // 是否实时显示错误？
         avatar: '',
         formdata: {
           skills:[],
@@ -198,27 +212,32 @@
           telephone: '',
           imgUpload: '',
         },
+        nextNum2: 1,
+        tips: []
       }
     },
     computed: {
       alerts () {
         // 表单校验
         // 是否首次输入
-        var pwd = null
-        var ok = true
-        var form = 'init form alert'
-        pwd = (this.formdata.pwd !== this.formdata.pwdconfirm)?'两次输入不一致':'1'
+        var username, pwd, email
+        username = (this.formdata.username === '') ? '用户名为空' : true
+        email = (this.formdata.email === '') ? '邮箱为空' : true
+        pwd = (this.formdata.pwd === '') ? '密码为空' : (this.formdata.pwd !== this.formdata.pwdconfirm) ? '两次输入不一致' : true
 
+        let data = {
+          username, pwd, email
+        }
+        let ok = Object.values(data).filter(v => v !== true).length === 0 
         return {
-          form,
           ok,
-          pwd
+          data
         }
       },
       regdata () {
         // 发起注册请求的data主体, 从this.formdata计算的来
         var password = null
-        if(this.alerts.pwd === '1'){
+        if(this.alerts.data.pwd === true){
           password = this.formdata.pwd
         }
         return {
@@ -234,24 +253,114 @@
     },
     methods: {
       register () {
-        this.init = false
-        this.submited = true
+        this.displayalerts = true
         if(this.alerts.ok === false){
           console.log('表单数据格式有误')
-          this.alerts.form = '表单数据格式有误'
+          this.toastOne({
+            type: 'danger',
+            message: '表单数据格式有误'
+          })
           return
         }
         console.group('注册')
         api.userReg((x)=>{
-          if (x.status === 0 ) {
-            this.msg = '注册成功'
-          }else{
-            this.msg = x.msg
+          let toastMsg = {
+            type: 'info',
+            message: '处理返回结果'
           }
-          console.log(x)
+          if (x.status === 0 ) {
+            toastMsg.type = 'success'
+            toastMsg.message = '注册成功, 跳转登陆'
+            // 登陆 然后跳转
+            api.userLogin((x)=>{
+              console.log(x)
+              if(x.status === 0){
+                console.log('登陆成功')
+                // 跳转到首页
+                this.$store.dispatch('setAuthed', true)
+                this.$store.dispatch('setUser', this.regdata.username)
+                setTimeout(()=>{
+                  this.$router.replace({ path: '/user/publish' })
+                }, 700)
+              } 
+            },{logindata: {
+              username: this.regdata.username,
+              password: this.regdata.password
+            }})
+          }else{
+            toastMsg.type = 'danger'
+            toastMsg.message = x.msg
+          }
+          // console.log(toastMsg)
+          this.toastOne(toastMsg)
         },{regdata:this.regdata})
         console.groupEnd()
+      },
+      closeIdx (idx) {
+        idx = idx || 0
+        this.tips.splice(idx, 1)
+      },
+      toastOne (tip) {
+        // showthenclose
+        // console.log(tip)
+        let index = this.add2(tip)
+        // console.log(index)
+        let _this = this
+        this.timers[index] = setTimeout(function(){
+          _this.closeIdx()
+          delete _this.timers[index]
+        },1500 + 700 * Object.keys(this.timers).length)
+      },
+      shiftTip () {
+        console.group('显示提示信息')
+        console.log('显示样式')
+        this.tips.shift()
+        console.groupEnd()
+      },
+      randomIndex2: function () {
+        return Math.floor(Math.random() * this.tips.length)
+      },
+      add2: function (tmptip) {
+         console.log(tmptip)
+        if (this.tips.length > 4) {
+          // console.log( this.tips.length + ' 大于4 ')
+          delete this.timers[Object.keys(this.timers)[0]]
+          this.shiftTip()
+        }
+        tmptip = tmptip || {
+          type: 'success',
+          message: '测试信息'
+        }
+        // 为message 添加唯一id
+        Object.assign(tmptip, {
+          id: this.nextNum2++
+        })
+        let idx = this.tips.length
+        this.tips.splice(idx, 0, tmptip)
+        return idx
+      },
+      remove2: function () {
+        this.tips.splice(this.randomIndex2(), 1)
       }
     }
   }
 </script>
+<style>
+.row.tip{
+  background: rgba(255, 255, 255, .5); 
+  position: fixed;
+  top:10%;
+  width: 60%;
+  left: 20%;
+}
+.a-complete-item{
+  transition: all 1s;
+}
+.a-complete-enter, .a-complete-leave-active {
+  opacity: 0;
+  transform: translateX(30px);
+}
+.a-complete-leave-active {
+  position: absolute;width: 100%;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -81,7 +81,7 @@ const routes = [{
 }]
 
 const router = new VueRouter({
-  mode: 'history',
+  /*mode: 'history',*/
   routes
 })
 /* eslint-disable no-new */
