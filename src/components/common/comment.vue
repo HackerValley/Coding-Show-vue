808b5ab33630e8b78a8e4c755af59e6ae3c8e441
songlairui <cnre@live.cn>
2017-04-28 14:20:18 +0800

    使用localStorage 保存头像地址。超时时间2min

--- src/components/common/comment.vue
+++ src/components/common/comment.vue
@@ -23,7 +23,7 @@
     <div class="comments">
       <transition-group name='b-complete'>
         <div class="comment b-complete-item" v-for="(v, i) in comments" :key='v._id' :id='v._id'>
-          <div class="figure"><img data-src="holder.js/100%x180" alt="..." src="/static/assets/figure_blank_2.png">
+          <div class="figure"><img :data-dc='getFigure(v.user._id, v.user)' :alt="v.user.nickname || v.user._id" :src="v.user.avatar||'/static/assets/figure_blank_2.png'">
           </div>
           <div class="context">
             <span class="nick"><b><router-link :to='"/user/profile/" + v.user._id'>{{ v.user.nickname || v.user._id }}</router-link></b></span>
@@ -37,183 +37,221 @@
   </div>
 </template>
 <script>
-  import * as api from '../../api/request.js'
-  import {
-    mapActions
-  } from 'vuex'
+import * as api from '../../api/request.js'
+import {
+  mapActions
+} from 'vuex'
 
-  export default {
-    name: 'Comments',
-    data() {
-      return {
-        list: [],
-        wrap: {},
-        comments: [],
-        commentdata: '',
-        commenting: null,
-        loading:null,
-        page_num: 1,
-        page_total: 1,
-        page_size: 7,
-        record_total: 1,
-      }
+export default {
+  name: 'Comments',
+  data() {
+    return {
+      list: [],
+      wrap: {},
+      comments: [],
+      commentdata: '',
+      commenting: null,
+      loading: null,
+      page_num: 1,
+      page_total: 1,
+      page_size: 7,
+      record_total: 1,
+    }
+  },
+  beforeMount() {
+    this.init()
+  },
+  created() {
+    localStorage.setItem('test', 'here')
+  },
+  methods: {
+    init() {
+      // console.log(this.$route.params.id)
+      this.fetch({}, (result) => {
+        if (result) {
+          this.comments.push(...result.list)
+        }
+      })
     },
-    beforeMount() {
-      this.init()
-    },
-    methods: {
-      init() {
-        // console.log(this.$route.params.id)
-        this.fetch({}, (result) => {
-          if (result) {
-            this.comments.push(...result.list)
-          }
-        })
-      },
-      fetch({
+    fetch({
+        id,
+      page_num,
+      page_size
+      }, cb, errcb) {
+      id = id || this.$route.params.id
+      page_num = page_num || 1
+      page_size = page_size || this.page_size
+      errcb = errcb || function (err) { console.info(err) }
+      api.getProjComments({
         id,
         page_num,
         page_size
-      }, cb, errcb) {
-        id = id || this.$route.params.id
-        page_num = page_num || 1
-        page_size = page_size || this.page_size
-        errcb = errcb || function (err){console.info(err)}
-        api.getProjComments({
-          id,
-          page_num,
-          page_size
-        }, (err, x) => {
-          if (err) {
-            // console.error('错误', err)
-            errcb(err)
-            return null
-          } else {
-            this.page_num = x.data.page_num
-            this.page_total = x.data.page_total
-            cb(x.data)
-          }
-        })
-      },
-      loadmore() {
-        let target_page_num = this.page_num + 1
-        this.loading = 'loading'
-        this.fetch({
-          page_num: target_page_num
-        }, (result) => {
-          if (result) {
-            this.comments.push(...result.list)
-            this.loading = null
-          }
-        }, (err)=>{
-          console.error('err:',err)
-          this.loading = null
-        })
-      },
-      addComment() {
-        if(!this.$store.state.authed){
-          return this.newToast({
-            type:'danger',
-            message: '请先登录'
-          })
+      }, (err, x) => {
+        if (err) {
+          // console.error('错误', err)
+          errcb(err)
+          return null
+        } else {
+          this.page_num = x.data.page_num
+          this.page_total = x.data.page_total
+          cb(x.data)
         }
-        let comment_msg = this.commentdata
-        let p_id = this.$route.params.id
-        if (comment_msg.length < 4) {
+      })
+    },
+    loadmore() {
+      let target_page_num = this.page_num + 1
+      this.loading = 'loading'
+      this.fetch({
+        page_num: target_page_num
+      }, (result) => {
+        if (result) {
+          this.comments.push(...result.list)
+          this.loading = null
+        }
+      }, (err) => {
+        console.error('err:', err)
+        this.loading = null
+      })
+    },
+    addComment() {
+      if (!this.$store.state.authed) {
+        return this.newToast({
+          type: 'danger',
+          message: '请先登录'
+        })
+      }
+      let comment_msg = this.commentdata
+      let p_id = this.$route.params.id
+      if (comment_msg.length < 4) {
+        this.newToast({
+          type: 'danger',
+          message: '评论内容太少'
+        })
+        return
+      }
+      this.commenting = 1
+      api.addComments({
+        comment_msg,
+        p_id
+      }, (err, x) => {
+        if (err) {
+          // display some global error message
           this.newToast({
             type: 'danger',
-            message: '评论内容太少'
+            message: err
           })
-          return
-        }
-        this.commenting = 1
-        api.addComments({
-          comment_msg,
-          p_id
-        }, (err, x) => {
-          if (err) {
-            // display some global error message
+        } else {
+          if (x[0] === '<') {
             this.newToast({
               type: 'danger',
-              message: err
+              message: '尚未登录'
             })
+            this.commenting = null
+            return
           } else {
-            if (x[0] === '<') {
-              this.newToast({
-                type: 'danger',
-                message: '尚未登录'
-              })
-              this.commenting = null
-              return
-            } else {
-              // console.log(x)
-              this.commentdata = ''
-              this.comments.pop()
-              this.comments.unshift(x.date)
-              this.commenting = null
-            }
+            // console.log(x)
+            this.commentdata = ''
+            this.comments.pop()
+            this.comments.unshift(x.date)
+            this.commenting = null
           }
-        })
-      },
-      ...mapActions([
-        'newToast',
-        'setPage'
-      ])
+        }
+      })
     },
-    filters: {
-      showTime: function (value) {
-        if (!value) return ''
-        return new Date(value).toLocaleString()
+    getFigure(_id, user) {
+      // let vm = this
+      // return {}
+      // console.info(`_id:${_id}`)
+      let figures = {}
+      figures = JSON.parse(localStorage.figurepool || '{}')
+      // figures[_id] = { avatar: '/static/assets/figure_blank_2.png' }
+      // localStorage.figurepool = JSON.stringify(figures)
+      // console.info('figures', figures)
+      // console.info('figure_id',figures[_id])
+      // console.info(typeof figures[_id].createdTime)
+      // console.assert(!figures[_id])
+
+      if (!figures[_id] || typeof figures[_id].createdTime === 'undefined' || new Date() - figures[_id].createdTime > 2 * 60 * 1000) {
+        api.userInfo(_id, info => {
+          console.info(info)
+          figures[_id] = {
+            avatar: info.data.avatar,
+            createdTime: new Date()
+          }
+          localStorage.figurepool = JSON.stringify(figures)
+          // console.log(figures)
+          user.avatar = figures[_id].avatar
+          return 'ok'
+        })
       }
+      user.avatar = figures[_id].avatar
+      return 'pending'
+      // return { avatar: '/static/assets/figure_blank_2.png' }
+    },
+    ...mapActions([
+      'newToast',
+      'setPage'
+    ])
+  },
+  filters: {
+    showTime: function (value) {
+      if (!value) return ''
+      return new Date(value).toLocaleString()
     }
   }
+}
 
 </script>
 <style>
-  .comments {
-    padding-top: 2em;
-  }
+.comments {
+  padding-top: 2em;
+}
 
-  .comment {
-    min-height: 80px;
-    overflow: hidden;
-    line-height: 2em;
-    padding-bottom: 2em;
-  }
+.comment {
+  min-height: 80px;
+  overflow: hidden;
+  line-height: 2em;
+  padding-bottom: 2em;
+}
 
-  .comment .figure {
-    float: left;
-    height: 100%;
-    margin: 10px;
-  }
+.comment .figure {
+  float: left;
+  height: 100%;
+  margin: 10px;
+}
 
-  .comment .figure img {
-    width: 48px;
-    height: 48px;
-    border-radius: 50%;
-  }
+.comment .figure img {
+  width: 48px;
+  height: 48px;
+  border-radius: 50%;
+}
 
-  .comment .context {
-    padding-left: 80px;
-  }
+.comment .context {
+  padding-left: 80px;
+}
 
-  .comment .context span {
-    display: inline-block;
-  }
+.comment .context span {
+  display: inline-block;
+}
 
-  .comment .context .nick {}
+.comment .context .nick {}
 
-  .comment .context .content {
-    line-height: 1.4em;
-  }
-  .b-complete-item{
-    transition: all .3s;
-  }
-  .b-complete-enter, .b-complete-leave-active {
-    opacity: 0;
-    transform: translateX(30px);
-  }
-  .b-complete-leave-active {
-    position: absolute;width: 100%;
-  }
+.comment .context .content {
+  line-height: 1.4em;
+}
+
+.b-complete-item {
+  transition: all .3s;
+}
+
+.b-complete-enter,
+.b-complete-leave-active {
+  opacity: 0;
+  transform: translateX(30px);
+}
+
+.b-complete-leave-active {
+  position: absolute;
+  width: 100%;
+}
 </style>
